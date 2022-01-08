752a22205384975488b89046d00991f4687bebbb
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-01-08 13:10:18 +0100

    Install app in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.endpoints',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
